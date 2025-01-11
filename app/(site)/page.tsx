39bf746bcbea0c6055ac3c32153e3a91e65c5b49
Matt Davis <matthewpeterdavis@gmail.com>
2025-01-11 20:23:41 +0000

    update hero to pull data from CMS

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,11 +3,7 @@ import { About, Hero } from '@/components';
 export default function Home() {
   return (
     <>
-      <Hero
-        title="Connect with and champion Gazan families trying to survive genocide"
-        description="We are a collective of allies around the world who are connecting with families in Gaza as pen pals and champions for their fundraising campaigns to survive day-to-day life Gaza."
-      />
-
+      <Hero />
       <div className="mx-auto max-w-screen-lg">
         <About />
       </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,35 +1,35 @@
 import { Button } from '@/components'
 import Link from 'next/link';
+import { getContent } from '@/sanity/sanity.query';
+import { SiteContentType } from '@/types';
 
-import type { ReactNode } from 'react';
+const Hero = async () => {
+    const content: SiteContentType[] = await getContent()
 
+    return (
+        <div className="bg-hero bg-center bg-no-repeat bg-cover h-hero max-w-hero mx-auto w-full" >
+            <header className="text-left mx-auto max-w-screen-lg px-3 pt-8">
+                <h1 className="whitespace-pre-line text-2xl font-bold leading-hero text-gray-100">
+                    <span className="text-3xl text-gray-900">
+                        {content && content[0] && content[0].landingTitle}
+                    </span>
+                </h1>
+                <div className="mb-16 mt-4 text-xl text-gray-100">
+                    {content && content[0] && content[0].landingSubtitle}
+                </div>
 
-type HeroProps = {
-    title: ReactNode;
-    description: string;
+                <Link href="/volunteer">
+                    <Button text="Volunteer With Us" />
+                </Link>
+
+                <br /><br />
+                <Link href="/funds">
+                    <Button text="See the funds we&apos;re supporting" />
+                </Link>
+
+            </header>
+        </div>
+    )
 };
-
-const Hero = (props: HeroProps) => (
-    <div className="bg-hero bg-center bg-no-repeat bg-cover h-hero max-w-hero mx-auto w-full" >
-        <header className="text-left mx-auto max-w-screen-lg px-3 pt-8">
-            <h1 className="whitespace-pre-line text-2xl font-bold leading-hero text-gray-100">
-                <span className="text-3xl text-gray-900">
-                    {props.title}
-                </span>
-            </h1>
-            <div className="mb-16 mt-4 text-xl text-gray-100">{props.description}</div>
-
-            <Link href="/volunteer">
-                <Button text="Volunteer With Us" />
-            </Link>
-
-            <br /><br />
-            <Link href="/funds">
-                <Button text="See the funds we&apos;re supporting" />
-            </Link>
-
-        </header>
-    </div>
-);
 
 export default Hero
