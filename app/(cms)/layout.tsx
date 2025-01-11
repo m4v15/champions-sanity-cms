8cdf0a0666f89ab8fe9a257b5415e381bcdcaefb
Matt Davis <matthewpeterdavis@gmail.com>
2025-01-11 14:46:18 +0000

    faff about with the folder structure to seperate CMS and site stuff

---
+++ app/(cms)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Gaza Champions",
+  description:
+    'Initiative to find individuals to champion fundraising efforts from Gaza',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
