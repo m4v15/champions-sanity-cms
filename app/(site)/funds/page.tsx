6c517513067000f6e6fd871fcc5eb971fc4fbdc0
Matt Davis <matthewpeterdavis@gmail.com>
2025-01-11 15:22:23 +0000

    add funds page

---
+++ app/(site)/funds/page.tsx
@@ -0,0 +1,24 @@
+
+
+export default function Fundraisers() {
+    return (
+        <div className="text-gray-800 antialiased">
+            <div className="mx-auto max-w-screen-xl p-3">
+                <div className="w-full text-left">
+                    <div className=" text-2xl font-bold">Funds We&apos;re Supporting</div>
+                    <br />
+                    All of these funds come from people in Gaza we know personally, or
+                    else friends of people we know personally. Please support and donate
+                    to as many as you can. <br />
+                    <br />
+                    <p className="text-xs italic">
+                        Please note, the amount raised here may not update for a few hours
+                        after donations, although it will update on the GoFundMe page
+                        directly
+                    </p>
+                    <br />
+                </div>
+            </div>
+        </div>
+    );
+}
