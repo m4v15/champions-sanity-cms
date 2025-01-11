6c7c5d4f531d9eccc2840989ec602f420a6bef2d
Matt Davis <matthewpeterdavis@gmail.com>
2025-01-11 22:06:12 +0000

    use CMS data for links page

--- app/(site)/links/page.tsx
+++ app/(site)/links/page.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link';
+import { getLinks } from '@/sanity/sanity.query';
+import { SanityLinksType } from '@/types';
 
 import { Button, Section, ExternalLink } from '@/components'
-export default function Links() {
+export default async function Links() {
+    const links: SanityLinksType[] = await getLinks()
+
+    const projects = links && links.filter(link => link.type?.includes('project'))
+    const media = links && links.filter(link => link.type?.includes('media'))
 
     return <div className="text-gray-600 antialiased">
         <Section>
@@ -10,41 +16,36 @@ export default function Links() {
                     <div className="text-2xl font-bold">
                         Other Projects we are part of
                     </div>
-                    <br />
-                    <ExternalLink
-                        url="https://opencollective.com/mutual-aid-gaza/projects/emergency-fund-gsg"
-                        text="Gaza Mutual Aid fund - all money is sent directly to Palestinians in Gaza"
-                    />
-                    <br />
-                    <br />
-                    <ExternalLink
-                        url="https://gaza.online"
-                        text="Gaza Online - distributing e-sims to people in Gaza"
-                    />
-                    <br />
-                    <br />
-                    <ExternalLink
-                        url="https://www.instagram.com/gazamemorymap/"
-                        text="GazaMemoryMap roots Palestinian memory to land, affirming the inextinguishable truth that Gaza will always belong to the people of Palestine."
-                    />
-                    <br />
+                    {projects.map(project => {
+                        return (
+                            <>
+                                <br />
+                                <ExternalLink
+                                    key={project._id}
+                                    url={project.url}
+                                    text={project.text}
+                                />
+                                <br />
+                            </>)
+                    })}
                     <br />
                     <div className="text-2xl font-bold">
                         Gaza Fundraising Coverage in the Media
                     </div>
-                    <br />
-                    <ExternalLink
-                        url="https://hyperallergic.com/879012/raising-funds-to-help-samah-ramadan-family-of-10-exit-gaza/"
-                        text="HYPERALLERGIC - Raising Funds to Help a Family of 10 Exit Gaza"
-                    />
-                    <br />
-                    <br />
-                    <ExternalLink
-                        url="https://theintercept.com/2024/03/07/gaza-palestinians-border-crossing-egypt/"
-                        text="The Intercept - Desperate to escape Gaza carnage, Palestinians are
-                        forced to pay exorbitant fees to enter Egypt"
-                    />
+                    {media.map(media => {
+                        return (
+                            <>
+                                <br />
+                                <ExternalLink
+                                    key={media._id}
+                                    url={media.url}
+                                    text={media.text}
+                                />
+                                <br />
+                            </>)
+                    })}
                 </div>
+                <br />
                 <Link
                     href="https://docs.google.com/forms/d/e/1FAIpQLSdQBwxbU97VXa9Phi4ACSrQMfIJNp6ZFPfQvhAjJ2J1ekoMJg/viewform"
                     rel="noopener noreferrer"
