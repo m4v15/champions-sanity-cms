cd912a89bc16d63737f8bd60cd06542b8f790a52
Matt Davis <matthewpeterdavis@gmail.com>
2025-04-21 22:38:22 +0100

    front page and about page redesign

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,18 @@
+'use client'
+
+import { useState, useEffect } from "react";
+
 import Link from "next/link";
+import { InstagramEmbed } from 'react-social-media-embed';
+
 
 const Landing = () => {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   return (
     <div className="relative isolate overflow-hidden bg-white">
       <div className="flex flex-col-reverse lg:flex-row mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-20">
@@ -18,8 +30,9 @@ const Landing = () => {
           </p>
           <div className="mt-10 flex items-center gap-x-6">
             <Link
-              href="/volunteer"
-              className="rounded-md bg-theme-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-theme-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-theme-red-600"
+              rel="noopener noreferrer" target="_blank"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdQBwxbU97VXa9Phi4ACSrQMfIJNp6ZFPfQvhAjJ2J1ekoMJg/viewform"
+              className="rounded-md bg-theme-red-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-theme-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-theme-red-600"
             >
               Join us
             </Link>
@@ -33,6 +46,30 @@ const Landing = () => {
           </div>
         </div>
       </div>
+      {isClient &&
+        <>
+          <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:px-8 lg:py-2">
+
+            <h1 className="font-header pb-10 text-center lg:text-left text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+              Latest Posts
+            </h1>
+
+            <div className="max-w-7xl mx-auto flex flex-col items-center lg:items-start lg:flex-row justify-between pb-24">
+              <InstagramEmbed url="https://www.instagram.com/p/DH-9ZXYAvL1" width={328} />
+              <InstagramEmbed url="https://www.instagram.com/p/DInNPwmNDzx/" width={328} />
+              <InstagramEmbed url="https://www.instagram.com/p/DIfQaBMtta0/" width={328} />
+            </div>
+          </div>
+          <div className="bg-theme-white-100 flex justify-center">
+            <div className="w-[75dvw] lg:w-auto p-3 lg:p-0 lg:max-w-7xl flex flex-col lg:flex-row gap-2 items-center text-center lg:text-left justify-center">
+              <p className="lg:mb-10 text-pretty text-md font-medium text-gray-500 sm:text-lg/8">
+                Join our newsletter to hear about the families, events we run to support them, and news from our champions
+              </p>
+              <iframe src="https://gazachampions.substack.com/embed" width="480" height="150" ></iframe>
+            </div>
+          </div>
+        </>
+      }
     </div >
   )
 }
