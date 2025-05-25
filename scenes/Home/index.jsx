d26c40c583308e207199951d9fbe7a7bae518ad5
Matt Davis <matthewpeterdavis@gmail.com>
2025-05-25 22:08:31 +0100

    fix it again

--- scenes/Home/index.jsx
+++ scenes/Home/index.jsx
@@ -2,7 +2,7 @@ import { Hero, Insta, Substack } from "./components";
 
 const Home = () => {
   return (
-    <div className="relative isolate overflow-hidden bg-white grid grid-rows-[1fr_auto_auto] gap-1">
+    <div className="relative isolate overflow-hidden bg-white grid grid-rows-[100vh_auto_auto] gap-1">
       <Hero />
       <Substack />
       <Insta />
