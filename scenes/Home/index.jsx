b7623ff5e25d45bccd5ba4e6fbed22296b536a14
Matt Davis <matthewpeterdavis@gmail.com>
2025-05-26 11:23:20 +0100

    tidy home

--- scenes/Home/index.jsx
+++ scenes/Home/index.jsx
@@ -3,7 +3,7 @@ import { Faqs, Hero, Insta, Substack } from "./components";
 const Home = () => {
   return (
     <>
-      <div className="relative isolate overflow-hidden bg-white grid grid-rows-[100vh_auto_auto] gap-10  pb-14 lg:pb-[14rem] px-2 lg:px-14">
+      <div className="relative isolate overflow-hidden bg-white grid grid-rows-[90vh_auto_auto] gap-10  pt-8 lg:pt-0 pb-14 lg:pb-[14rem] px-2 lg:px-14">
         <Hero />
         <Insta />
         <Faqs />
