d04a905ede370a8b194417aafd695cd1d621245f
Matt Davis <matthewpeterdavis@gmail.com>
2025-07-12 10:53:38 +0100

    add way to hide substack

--- scenes/Home/components/Substack.tsx
+++ scenes/Home/components/Substack.tsx
@@ -4,13 +4,14 @@ import { useState, useEffect } from "react";
 
 const Substack = () => {
     const [isClient, setIsClient] = useState(false);
+    const [hide, setHide] = useState(false);
 
     useEffect(() => {
         setIsClient(true);
     }, []);
 
     return (
-        isClient && (
+        isClient && !hide && (
             <>
                 <div className="bg-theme-white-100 h-[8rem] py-3 lg:fixed lg:bottom-0 lg:w-full ">
                     <div className="flex flex-col lg:flex-row gap-2 items-center text-center lg:text-left justify-center">
@@ -24,6 +25,25 @@ const Substack = () => {
                             height="150"
                         ></iframe>
                     </div>
+                    <button
+                        className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                        onClick={() => setHide(true)}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </button>
                 </div>
             </>
         )
