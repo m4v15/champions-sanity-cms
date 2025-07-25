b7c18b5dc8f41c4a1f992c6d463dda2608f0dfab
Matt Davis <matthewpeterdavis@gmail.com>
2025-07-25 15:53:43 +0100

    fix FAQ button link

--- app/(site)/faq/page.tsx
+++ app/(site)/faq/page.tsx
@@ -1,15 +1,19 @@
-
+import Link from "next/link";
 import { Button } from "@/components";
 import { Faqs } from "@/scenes/Home/components";
-
-
 
 export default function Home() {
   return (
     <div className="mx-auto py-8 px-3 lg:px-14 grid gap-10">
       <Faqs />
       <div className="mx-auto">
-        <Button text="Become A Champion" />
+        <Link
+          href="https://docs.google.com/forms/d/e/1FAIpQLSdQBwxbU97VXa9Phi4ACSrQMfIJNp6ZFPfQvhAjJ2J1ekoMJg/viewform"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Button text="Become A Champion" />
+        </Link>
       </div>
     </div>
   );
